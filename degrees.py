dbf4e30c86415bb60b2a2685fc91a395345768f5
gituser-sudo <48635872+gituser-sudo@users.noreply.github.com>
2026-02-27 20:16:14 -0600

    updated degrees

--- degrees.py
+++ degrees.py
@@ -2,6 +2,9 @@ import csv
 import sys
 
 from util import Node, StackFrontier, QueueFrontier
+from dataclasses import dataclass
+from typing import Tuple, FrozenSet
+from collections import deque
 
 # Maps names to a set of corresponding person_ids
 names = {}
@@ -12,6 +15,20 @@ people = {}
 # Maps movie_ids to a dictionary of: title, year, stars (a set of person_ids)
 movies = {}
 
+# Define a simple (movie_id, actor_id) pair
+@dataclass(frozen=True)
+class MovieActor:
+    movie_id: int
+    actor_id: int
+
+
+# Define the main object that will go inside a Python set
+@dataclass(frozen=True)
+class Node:
+    depth: int
+    obj: MovieActor
+    list_of_objects: Tuple[MovieActor, ...]   # immutable instead of list
+    set_of_actor_ids: FrozenSet[int]          # immutable instead of set
 
 def load_data(directory):
     """
@@ -92,8 +109,115 @@ def shortest_path(source, target):
     If no possible path, returns None.
     """
 
+    # maintain a set for each depth
+    # each object in the set will have the current (movie, actor) pair
+    # the consolidated set of movie, actor beginning from root. so depth 0 will have 1 item, depth 1 will have 2 items etc
+    # list of actor ids from source. if the new actor id is target we reached a solution and return the consolidated set
+    # if the new actor id exists in the list of actor ids then we reached a cyclic solution . we need to try the next item in the
+    # level. if we exaust all items in the level and they are open we move on to next depth
+    # advantages of breath first here is any solution reached is optimum
+
+    # create a depth 0 set of nodes from the neighbors of person returned
+
+    source_id = person_id_for_name(source)
+    target_id = person_id_for_name(target)
+    node_map
+
+    # initial source node
+
+    node_source = Node(
+    depth= -1,
+    obj= None,
+    list_of_objects = [],
+    set_of_actor_ids = frozenset()
+    )
+    node_map[-1][0] = node_source
+    current_depth = 0
+
+    while true
+
+        # iterate through the node_list at current depth - 1
+
+        iterating_depth = depth - 1
+        for node in node_map[iterating_depth]
+            neighbors_list = neighbors_for_person(current_id)
+
+            index = 0
+            for neighbor in neighbors_list
+                # create the node list for all the neighbors for this depth 0
+                node_item = Node(
+                depth= depth,
+                obj=neighbor,
+                list_of_objects = node("list_of_objects") + (neighbor),
+                set_of_actor_ids = node("set_of_actor_ids") + frozenset({source_id})
+                )
+
+                current_id = neighbor[1]
+                node_map[depth] = node
+                if current_id = target_id
+                return list_of_objects
+                if current_id not in set_of_actor_ids:
+                    node_map[depth][index] = node
+
+        depth = depth + 1
+
+
+
+
     # TODO
     raise NotImplementedError
+
+
+
+def shortest_path(source, target):
+    """
+    Returns the shortest list of (movie_id, person_id) pairs
+    that connect the source to the target.
+    If no possible path, returns None.
+    """
+    source_id = person_id_for_name(source)
+    target_id = person_id_for_name(target)
+
+    # BFS queue
+    queue = deque()
+
+    # Initial node
+    node_source = Node(
+        depth=0,
+        obj=None,
+        list_of_objects=[],
+        set_of_actor_ids=frozenset({source_id})
+    )
+    queue.append(node_source)
+
+    while queue:
+        current_node = queue.popleft()
+        current_depth = current_node.depth
+        current_actor_id = list(current_node.set_of_actor_ids)[-1]  # last actor in path
+
+        for neighbor in neighbors_for_person(current_actor_id):
+            movie_id, person_id = neighbor
+
+            # Avoid cycles
+            if person_id in current_node.set_of_actor_ids:
+                continue
+
+            # Build new node
+            new_node = Node(
+                depth=current_depth + 1,
+                obj=neighbor,
+                list_of_objects=current_node.list_of_objects + [neighbor],
+                set_of_actor_ids=current_node.set_of_actor_ids.union({person_id})
+            )
+
+            # If we reached target, return path
+            if person_id == target_id:
+                return new_node.list_of_objects
+
+            queue.append(new_node)
+
+    # No path found
+    return None
 
 
 def person_id_for_name(name):
@@ -133,6 +257,7 @@ def neighbors_for_person(person_id):
         for person_id in movies[movie_id]["stars"]:
             neighbors.add((movie_id, person_id))
     return neighbors
+
 
 
 if __name__ == "__main__":
